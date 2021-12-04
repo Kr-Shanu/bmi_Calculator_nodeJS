fc851d6fe169c537d0599b3338830d5db8b03335
Kr-Shanu <80747792+Kr-Shanu@users.noreply.github.com>
2021-12-04 23:07:53 +0530

    Add files via upload

---
+++ bmiCalculator.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/bmicalculator", function(req, res)
+{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/bmicalculator", function(req, res)
+{
+    var weight = Number(req.body.w);
+    var height = Number(req.body.h);
+    var bmi = weight/(height*height);
+    res.send("The BMI of your body is: "+bmi)
+});
+
+app.listen(3000, function()
+{
+    console.log("Server started working");
+});
